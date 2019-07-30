c2daecef6f18584cd58365a80293bbed35cda93a
hwpwk <34832156+hwpwk@users.noreply.github.com>
2019-07-30 22:01:16 +0900

    Update summary_my_create_function.py

--- summary_my_create_function.py
+++ summary_my_create_function.py
@@ -721,7 +721,6 @@ def calc_groupby_median(df, col1, col2, main_col):
     # 件数を降順にする
     df = df.reset_index().sort_values('件数', ascending=False)
 
-    
     return df
 
 def create_mean_pivot_table(df, index, columns, values):
