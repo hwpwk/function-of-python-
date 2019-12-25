3bd6ba05ee5a24ad6de194b61e79fbac2be843be
hwpwk <34832156+hwpwk@users.noreply.github.com>
2019-12-25 20:42:35 +0900

    Update summary_my_create_function.py

--- summary_my_create_function.py
+++ summary_my_create_function.py
@@ -1,3 +1,31 @@
+def col_data_in_dataframe(df):
+    '''
+    関数内容
+    ・データフレームのカラム名毎の「データ型」「欠損値数」「欠損率」「ユニーク数」「ユニーク率」をまとめたデータフレームを作成する関数
+    Input
+    ・df：データフレーム
+    関数使用方法
+    ・col_data_in_dataframe(df)
+    '''
+    
+    col_list = df.columns.tolist()
+    null_num_list = df.isnull().sum().tolist()
+    data_type_list = df.dtypes.tolist()
+    col_unique_num_list = df.nunique().tolist()
+    null_num_per_list = [str(round(num/df.shape[0],2)*100)+'%' for num in null_num_list]
+    unique_num_per_list = [str(round(num/df.shape[0],2)*100)+'%' for num in col_unique_num_list]
+
+    summary_df = pd.DataFrame({
+        'カラム名':col_list,
+        'データ型':data_type_list,    
+        '欠損値数':null_num_list,
+        '欠損率':null_num_per_list,
+        'ユニーク数':col_unique_num_list,
+        'ユニーク率':unique_num_per_list
+    })
+
+    return summary_df
+
 def basic_check(df):
     '''
     関数内容
